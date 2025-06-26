152c1d60153e656247940d9522479c6bf928051e
Harrison <hday2002@gmail.com>
2025-06-26 15:42:46 -0500

    move assets folder to bin when building

--- src/frontend/ui.hpp
+++ src/frontend/ui.hpp
@@ -6,13 +6,60 @@ using namespace uilo;
 void loop() {
     UILO ui("MULO", {{
         page({
-            row(
+            column(
                 Modifier()
-                    .setColor(sf::Color::White)
+                    .setColor(sf::Color(25, 25, 25))
                     .setHeight(1.f)
                     .setWidth(1.f),
             contains{
                 // Add your UI elements here
+                row(
+                    Modifier()
+                        .setColor(sf::Color(210, 210, 210))
+                        .setWidth(1.f)
+                        .setfixedHeight(64)
+                        .onClick([&]() {
+                            std::cout << "Bottom row clicked!" << std::endl;
+                        }),
+                contains{
+
+                }),
+
+                column(
+                    Modifier()
+                        .setColor(sf::Color(200, 200, 200))
+                        .setfixedWidth(256)
+                        .setHeight(1.f)
+                        .align(Align::LEFT)
+                        .onClick([&]() {
+                            std::cout << "Left column clicked!" << std::endl;
+                        }),
+                contains{
+                    spacer(Modifier().setfixedHeight(16)),
+
+                    text(
+                        Modifier()
+                            .setColor(sf::Color::Black)
+                            .setfixedHeight(16)
+                            .align(Align::TOP | Align::CENTER_X),
+                        "FILES",
+                        "assets/fonts/SpaceMono-Regular.ttf"
+                    ),
+
+                    spacer(Modifier().setfixedHeight(16)),
+                }),
+
+                row(
+                    Modifier()
+                        .setColor(sf::Color(210, 210, 210))
+                        .setWidth(1.f)
+                        .setfixedHeight(256)
+                        .onClick([&]() {
+                            std::cout << "Bottom row clicked!" << std::endl;
+                        }),
+                contains{
+
+                })
             })
         }), "base" }
     });
@@ -21,4 +68,14 @@ void loop() {
         ui.update();
         ui.render();
     }
+
+    // Text(
+    //     Modifier()
+    //         .setColor(sf::Color::Black)
+    //         .setfixedHeight(32)
+    //         .setfixedWidth(256)
+    //         .align(Align::CENTER_X | Align::CENTER_Y),
+    //     "Left Column",
+    //     "assets/fonts/SpaceMono-Regular.ttf"
+    // );
 }
