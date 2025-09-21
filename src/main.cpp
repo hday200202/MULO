73af275d7b30a4b3ed0669a09ae1a14b36f737ec
Prashant <Prashantneupane2004@gmail.com>
2025-09-21 01:49:05 -0500

    Refactor main.cpp to simplify JUCE applicatio-- START_JUCE_APPLICATION macro

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,4 @@
 #include "frontend/Application.hpp"
-#include <juce_events/juce_events.h>
 
-int main() {
-    juce::MessageManager::getInstance();
-    
-    Application app;
-    app.initialise(juce::String());
-
-    while (app.isRunning()) {
-        // Process JUCE messages in smaller chunks to avoid blocking SFML
-        juce::MessageManager::getInstance()->runDispatchLoopUntil(5);
-        app.update();
-        app.render();
-    }
-
-    app.shutdown();
-    juce::MessageManager::deleteInstance();
-    
-    return 0;
-}
+// JUCE Application Startup
+START_JUCE_APPLICATION(Application)
