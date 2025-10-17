411494374a3352741b42f6be4316677cfcc16c13
Lily <ndd232@gmail.com>
2025-10-17 16:19:25 -0500

    Added row for the decibel component

--- extensions_toolkit/extensions_src/DecibelComponent.hpp
+++ extensions_toolkit/extensions_src/DecibelComponent.hpp
@@ -17,13 +17,28 @@ private:
 };
 
 void DecibelComponent::init() {
+    decReaderRow = row(
+        Modifier()
+            .setWidth(1.f)
+            .setfixedHeight(32)
+            .align(Align::CENTER_X | Align::CENTER_Y)
+            .setColor(app->resources.activeTheme->track_color),
+        contains{}, "decReader_row"
 
+    layout = row(
+        Modifier()
+            .align(Align::LEFT | Align::TOP)
+            .setfixedHeight(48.f)
+            .setColor(app->resources.activeTheme->track_color),
+    contains{
+        decReaderRow,
+    );
 }
 
 bool DecibelComponent::handleEvents() {
-
+    return false;
 }
 
 void DecibelComponent::update() {
-    
+
 }
